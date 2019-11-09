a271ef1bf33e48064a1399ddb60848ec2fdb35b4
Han <xsytby1112@gmail.com>
2019-11-09 17:54:07 +0800

    docs: 修改文档, element 官方的信息
    docs: add installation
    
    chore: update lockfile
    
    chore: add standard-version
    
    chore: use femessage default issue template
    
    chore(README.md): replace some link from element to femessage
    
    docs(site): keep latest version only in our site
    
    docs(site): 修复中文文档顶部导航问题
    
    chore: keep site version same with element official
    
    ci: test

--- build/bin/version.js
+++ build/bin/version.js
@@ -1,8 +1,6 @@
 var fs = require('fs');
 var path = require('path');
 var version = process.env.VERSION || require('../../packages/theme-chalk/package.json').version;
-console.log('++++++++++++');
-console.log(version);
 var content = {};
 if (!content[version]) content[version] = '2.12';
 fs.writeFileSync(path.resolve(__dirname, '../../examples/versions.json'), JSON.stringify(content));
