2671931a995508106c343fa1b0b7ac468e72185b
Han <xsytby1112@gmail.com>
2019-11-09 16:38:38 +0800

    chore: notify use

--- notify.sh
+++ notify.sh
@@ -5,12 +5,12 @@ then
   exit 1
 fi
 
-git remote add github https://$GITHUB_TOKEN@github.com/FEMessage/vant.git > /dev/null 2>&1
+git remote add github https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG.git > /dev/null 2>&1
 git push github HEAD:master --follow-tags
 
 GREN_GITHUB_TOKEN=$GITHUB_TOKEN yarn gren
 
-url=https://api.github.com/repos/FEMessage/vant/releases/latest
+url=https://api.github.com/repos/$TRAVIS_REPO_SLUG/releases/latest
 resp_tmp_file=resp.tmp
 
 curl -H "Authorization: token $GITHUB_TOKEN" $url > $resp_tmp_file
@@ -19,7 +19,7 @@ html_url=$(sed -n 5p $resp_tmp_file | sed 's/\"html_url\"://g' | awk -F '"' '{pr
 body=$(grep body < $resp_tmp_file | sed 's/\"body\"://g;s/\"//g')
 version=$(echo $html_url | awk -F '/' '{print $NF}')
 
-msg='{"msgtype": "markdown", "markdown": {"title": "vant更新", "text": "@所有人\n# [vant('$version')]('$html_url')\n'$body'"}}'
+msg='{"msgtype": "markdown", "markdown": {"title": "element更新", "text": "@所有人\n# [vant('$version')]('$html_url')\n'$body'"}}'
 
 curl -X POST https://oapi.dingtalk.com/robot/send\?access_token\=$DINGTALK_ROBOT_TOKEN -H 'Content-Type: application/json' -d "$msg"
 
